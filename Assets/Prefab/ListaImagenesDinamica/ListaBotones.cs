20840a363b08cc4ab0a101361ee45a54bb6011c1
androidsyntelli <Taidy.Marrungo@syntelli.com>
2020-03-13 11:15:22 -0500

    muestra primer item en panel contenedor de lista de iconos

---
+++ Assets/Prefab/ListaImagenesDinamica/ListaBotones.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ListaBotones : MonoBehaviour
+{
+    public GameObject Lista;
+    public GameObject itemLista;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject item = Instantiate(itemLista,new Vector3(0f,0f,0f),Quaternion.identity);
+        item.transform.SetParent(Lista.transform,false);
+        
+    }
+
+}
