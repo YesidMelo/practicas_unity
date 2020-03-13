3a96f323b06f12400f3292c9a35d2b40823f013b
androidsyntelli <Taidy.Marrungo@syntelli.com>
2020-03-13 11:34:45 -0500

    Muestra varios items de lista en vista, aun no ha implementato el incremento de tamaño en el panel contenedor

--- Assets/Prefab/ListaImagenesDinamica/ListaBotones.cs
+++ Assets/Prefab/ListaImagenesDinamica/ListaBotones.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class ListaBotones : MonoBehaviour
 {
     public GameObject Lista;
@@ -11,9 +10,25 @@ public class ListaBotones : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameObject item = Instantiate(itemLista,new Vector3(0f,0f,0f),Quaternion.identity);
+        cargarListaIconos();
+    }
+
+    private void cargarListaIconos(){
+        int numeroItemsMock = 3;
+        float posicionX = 0;
+        float posicionY = 0;
+
+        for(int contador = 0; contador < numeroItemsMock ; contador ++ ){
+            RectTransform objectTransform = crearItemListaIcono(posicionX,posicionY).transform as RectTransform ;
+            posicionY -= objectTransform.rect.height;
+        }
+        Debug.Log("Alto final "+posicionY);
+    }
+
+    private GameObject crearItemListaIcono(float posicionX = 0f,float posicionY = 0f) {
+        GameObject item = Instantiate(itemLista,new Vector3(posicionX,posicionY,0f),Quaternion.identity);
         item.transform.SetParent(Lista.transform,false);
-        
+        return item;
     }
 
 }
