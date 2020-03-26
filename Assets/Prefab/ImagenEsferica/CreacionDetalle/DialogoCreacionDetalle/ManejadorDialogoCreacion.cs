8025cccc8316e4c0f782e4e66c3e03701837d424
Yesid <trigoperfec3@hotmail.com>
2020-03-26 13:32:31 -0500

    Creacion de interfaz del dialogo de creacion de comentario

--- Assets/Prefab/ImagenEsferica/CreacionDetalle/DialogoCreacionDetalle/ManejadorDialogoCreacion.cs
+++ Assets/Prefab/ImagenEsferica/CreacionDetalle/DialogoCreacionDetalle/ManejadorDialogoCreacion.cs
@@ -12,18 +12,41 @@ public class ManejadorDialogoCreacion : MonoBehaviour
     public Image Mantenimiento;
     public Image Parabolica;
 
+    public Image ImagenSeleccionada;
+    private Sprite spriteSeleccionado;
+
     private void Start() {
         poneEscuchadores();
     }
 
     private void poneEscuchadores(){
 
-        Agua.GetComponent<ManejadorClickIcono>().conEscuchador((string nombre)=>{ Debug.Log(nombre); });
-        Luz.GetComponent<ManejadorClickIcono>().conEscuchador((string nombre)=>{ Debug.Log(nombre); });
-        Gas.GetComponent<ManejadorClickIcono>().conEscuchador((string nombre)=>{ Debug.Log(nombre); });
-        Mantenimiento.GetComponent<ManejadorClickIcono>().conEscuchador((string nombre)=>{ Debug.Log(nombre); });
-        Parabolica.GetComponent<ManejadorClickIcono>().conEscuchador((string nombre)=>{ Debug.Log(nombre); });
-        
+        Agua.GetComponent<ManejadorClickIcono>().conEscuchador((string nombre)=>{ 
+            spriteSeleccionado = Agua.GetComponent<Image>().sprite;
+            ImagenSeleccionada.GetComponent<Image>().sprite = spriteSeleccionado;
+        });
+
+        Luz.GetComponent<ManejadorClickIcono>().conEscuchador((string nombre)=>{ 
+            spriteSeleccionado = Luz.GetComponent<Image>().sprite;
+            ImagenSeleccionada.GetComponent<Image>().sprite = spriteSeleccionado;
+        });
+
+        Gas.GetComponent<ManejadorClickIcono>().conEscuchador((string nombre)=>{ 
+            spriteSeleccionado = Gas.GetComponent<Image>().sprite;
+            ImagenSeleccionada.GetComponent<Image>().sprite = spriteSeleccionado;
+        });
+
+        Mantenimiento.GetComponent<ManejadorClickIcono>().conEscuchador((string nombre)=>{ 
+            spriteSeleccionado = Mantenimiento.GetComponent<Image>().sprite;
+            ImagenSeleccionada.GetComponent<Image>().sprite = spriteSeleccionado;
+        });
+
+        Parabolica.GetComponent<ManejadorClickIcono>().conEscuchador((string nombre)=>{ 
+            spriteSeleccionado = Parabolica.GetComponent<Image>().sprite;
+            ImagenSeleccionada.GetComponent<Image>().sprite = spriteSeleccionado;
+        });
+
+
     }
 
     // Update is called once per frame
